6d84b5dfd47f0d426ced45323ebdd27239ccbdf5
Xin Kai Wang <xk4wang@uwaterloo.ca>
2016-11-01 13:56:10 -0400

    Renames

--- src/Matcher.java
+++ src/Matcher.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Match
+public class Matcher
 {
 	public static void match(PriorityQueue<Person> single)
 	{
--- src/Scorer.java
+++ src/Scorer.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Scoring
+public class Scorer
 {
 	public static void scoring(Person[] people)
 	{
